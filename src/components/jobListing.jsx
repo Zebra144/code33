9d6738ef61b8db23b11e1d1f831caaf6bc0224a1
Zebra144 <sakybaky620@gmail.com>
2025-08-20 20:40:55 +0600

    updating job list

--- src/components/jobListing.jsx
+++ src/components/jobListing.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { AppContext } from '../context/AppContext'
 
 const jobListing = () => {
+  const{isSearched, searchedFilter} = useContext(Appcontext)
   return (
     <div>
-        {   }
+        {   /* Sidebar */}
+        <div>
+          {/* Search Filter From Hero Content */}
+          {
+              isSearched && (searchedFilter.title !== "" || searchedFilter.location !=="" ) && (
+                <>
+                <h3>Current Search</h3>
+                <div>
+                  {searchedFilter.title && (
+                    <span>
+                        {searchedFilter.title}
+                    </span>
+                  )}
+                  {searchedFilter.location && (
+                    <span>
+                      {searchedFilter.location}
+                    </span>
+                  )}
+                </div>
+                </>
+              )
+          }
+        </div>
     </div>
   )
 }
