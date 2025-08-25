23295163e6148fa1247b1a9f04376850c5c7deee
Zebra144 <sakybaky620@gmail.com>
2025-08-25 20:57:09 +0600

    adding locations and job list

--- src/components/jobListing.jsx
+++ src/components/jobListing.jsx
@@ -1,11 +1,13 @@
 import React, { useContext } from 'react'
 import { AppContext } from '../context/AppContext'
-import { assets, JobCategories } from '../assets/assets'
+import { assets, JobCategories,JobLocations,jobsData } from '../assets/assets'
+import JobCard from './JobCard'
+//import { assets, JobLocations } from '../assets/assets'
 
 const JobListing = () => {
   const{isSearched, searchFilter, setSearchFilter} = useContext(AppContext)
   return (
-    <div className='container  2xl:px-20 mx-auto flex flex-col lg: flex-row max-lg:space-y-8 py-8'>
+    <div className='container  2xl:px-20 mx-auto flex flex-col lg:flex-row max-lg:space-y-8 py-8'>
        
         <div className='w-full lg:w-1/4 bg-white px-4'>
           
@@ -39,7 +41,7 @@ const JobListing = () => {
               {
                 JobCategories.map((category,index)=>(
                   <li className='flex gap-3 items-center' key={index}>
-                    <input type="checkbox" name="" id="" />
+                    <input className='scale-125' type="checkbox" name="" id="" />
                     {category}
                   </li>
                 ))
@@ -48,7 +50,37 @@ const JobListing = () => {
           </div>
 
 
+              <div className='max-lg:hidden'>
+            <h4 className='font-medium text-lg py-4 pt-14'>
+              Search by Location
+            </h4>
+            <ul className='space-y-4 text-gray-600'>
+              {
+                JobLocations.map((location,index)=>(
+                  <li className='flex gap-3 items-center' key={index}>
+                    <input className='scale-125' type="checkbox" name="" id="" />
+                    {location}
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+
+
         </div>
+
+             
+              <section className='w-full lg:w-3/4 text-gray-800 max-lg:px-4'>
+                <h3 className='font-medium text-3xl py-2' id='job-list'>Latest Jobs</h3>
+                <p className='mb-8'>Get your desired Job from top companies</p>
+                <div className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-4'>
+                  {jobsData.map((job,index)=>(
+                    <JobCard key={index} job = {job}/>
+                  ))}
+                </div>
+              </section>
+
+
     </div>
   )
 }
