9f00a505471b800c36c7ccce06071dbfbfd91bb0
Abesh134 <abeshbarua2004@gmail.com>
2025-08-29 23:11:08 +0600

    job description page design css and etc

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -6,6 +6,8 @@ import Loading from '../components/Loading'
 import Navbar from '../components/Navbar'
 import kConvert from 'k-convert'
 import moment from 'moment'
+import JobCard from '../components/JobCard'
+import Footer from '../components/Footer'
 
 const ApplyJob = () => {
 
@@ -67,8 +69,31 @@ const ApplyJob = () => {
             </div >
 
           </div>
+
+          <div className='flex flex-col lg:flex-row justify-between items-start'>
+            <div className='w-full lg:w-2/3'>
+              <h2 className='font-bold text-2xl mb-4'>
+                Job Description
+                </h2>
+              <div className='rich-text' dangerouslySetInnerHTML={{ __html: jobData.description }}>
+              </div>
+              <button className='bg-blue-600 p-2.5 px-10 text-white rounded mt-10'>
+                Apply Now
+                </button>
+            </div>
+            <div className='w-full lg:w-1/3 mt-8 lg:mt-0 lg:ml-8 space-y-5'>
+              <h2>More Jobs From {jobData.companyId.name}</h2>
+              {jobs.filter(job => job._id !== jobData._id && job.companyId._id === jobData.companyId._id)
+              .filter( job => true).slice(0,4)
+              .map((job,index)=> <JobCard key={index} job={job}/>)}
+            </div>
+          </div>
+
+
+
         </div>
       </div>
+      <Footer/>
     </>
   ) : (
     <Loading />
