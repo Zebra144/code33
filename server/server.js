2cc0c21405999d8a27e91a1bb56b4f38c634d213
Zebra144 <sakybaky620@gmail.com>
2025-09-04 22:53:32 +0600

    server

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => res.send('API Working!!'))
+app.get('/', (req, res) => res.send('API Working!! '))
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
